990480e5ecd125b0608476b2b5d7e5b21db67b5f
Abinav Seelan <abinav.n.seelan@gmail.com>
2018-01-27 15:46:32 +0530

    Enable cors

--- server/index.js
+++ server/index.js
@@ -7,6 +7,12 @@ const routes = require('./routes');
 const app = express();
 const port = process.env.PORT || 1337;
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 if (process.env.NODE_ENV !== 'test') {
   app.use(morgan('dev'));
 }
