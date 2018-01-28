6e3250dc6e3aadecaca3137f403c4a35204afd3a
Aditi Mohanty <rheaditi@users.noreply.github.com>
2018-01-28 16:46:14 +0530

    Sorry </3

--- client/index.js
+++ client/index.js
@@ -1,8 +1,7 @@
+/* global notebook */
 const exampleContainer = document.getElementById('code-snippet');
-const codeHeader = document.getElementById('code-header');
 
 let noteBook; // eslint-disable-line
-
 document.addEventListener('DOMContentLoaded', () => {
   // eslint-disable-next-line
   notebook = RunKit.createNotebook({
@@ -13,36 +12,25 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // eslint-disable-next-line
 const changeExample = (range) => {
-  exampleContainer.innerHTML = '';
   let content;
   switch (range) {
     case '2': {
       content = 'const request = require(\'axios\');\n\n// Replace \'xx\' with any number you require\nrequest\n\t.get(\'http://127.0.0.0:1337/status-code/2xx\')\n\t.then((response) => {\n\t\tconsole.log(response)\n\t})\n\t.catch(() => {});';
-
-      codeHeader.innerText = 'Success response codes (2xx)';
       break;
     }
 
     case '3': {
-      content = `
-            const request = require('axios');\n\n// Replace 'xx' with any number you require\nrequest\n\t.get('http://127.0.0.0:1337/status-code/3xx')\n\t.then(() => {\n\t\t// no-op since this response code will trigger the catch block\n\t})\n\t.catch((err) => {\n\t\tconsole.log(err);\n\t});
-          `;
-
-      codeHeader.innerText = 'Redirect response codes (3xx)';
+      content = 'const request = require(\'axios\');\n\n// Replace \'xx\' with any number you require\nrequest\n\t.get(\'http://127.0.0.0:1337/status-code/3xx\')\n\t.then(() => {\n\t\t// no-op since this response code will trigger the catch block\n\t})\n\t.catch((err) => {\n\t\tconsole.log(err);\n\t});';
       break;
     }
 
     case '4': {
       content = 'const request = require(\'axios\');\n\n// Replace \'xx\' with any number you require\nrequest\n\t.get(\'http://127.0.0.0:1337/status-code/4xx\')\n\t.then(() => {\n\t\t// no-op since this response code will trigger the catch block\n\t})\n\t.catch((err) => {\n\t\tconsole.log(err);\n\t});';
-
-      codeHeader.innerText = 'Client error response codes (4xx)';
       break;
     }
 
     case '5': {
       content = 'const request = require(\'axios\');\n\n// Replace \'xx\' with any number you require\nrequest\n\t.get(\'http://127.0.0.0:1337/status-code/5xx\')\n\t.then(() => {\n\t\t// no-op since this response code will trigger the catch block\n\t})\n\t.catch((err) => {\n\t\tconsole.log(err);\n\t});';
-
-      codeHeader.innerText = 'Server error response codes (5xx)';
       break;
     }
 
@@ -51,11 +39,9 @@ const changeExample = (range) => {
     }
   }
 
-  // eslint-disable-next-line
-  notebook = RunKit.createNotebook({
-    element: exampleContainer,
-    source: content,
-  });
+  if (notebook && typeof notebook.setSource === 'function') {
+    notebook.setSource(content);
+  }
 };
 
 new Clipboard('.link'); // eslint-disable-line
--- gulpfile.js
+++ gulpfile.js
@@ -3,13 +3,14 @@ const sass = require('gulp-sass');
 const cleanCSS = require('gulp-clean-css');
 const nodemon = require('gulp-nodemon');
 
-gulp.task('sass', () => gulp.src('./client/*.scss').pipe(sass()).pipe(cleanCSS({ compatibility: 'ie8' })).pipe(gulp.dest('./client')));
+gulp.task('sass:dev', () => gulp.src('./client/*.scss').pipe(sass()).pipe(gulp.dest('./client')));
+gulp.task('sass:prod', () => gulp.src('./client/*.scss').pipe(sass()).pipe(cleanCSS({ compatibility: 'ie8' })).pipe(gulp.dest('./client')));
 
 gulp.task('start', () => {
   nodemon({
     script: 'server/index.js',
     ext: 'js html scss',
-    tasks: ['sass'],
+    tasks: ['sass:dev'],
     env: { NODE_ENV: 'development', PORT: 1337 },
   });
 });
